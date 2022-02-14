4a9feed1c91efdc5e6d38b8299d630a668e2907c
p.yasinskiy <pachazhele@gmail.com>
2022-02-14 03:45:06 +0300

    mod 2 homework

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
-
+from random import choice
 from django.views import View
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
+        for_rand = ['<li>Установить python</li>', '<li>Установить django</li>', '<li>Запустить сервер</li>',
+           '<li>Порадоваться результату</li>']
         return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
+                            f'{choice(for_rand)}'
+                            f'{choice(for_rand)}'
+                            f'{choice(for_rand)}'
+                            f'{choice(for_rand)}'
                             '</ul>')
