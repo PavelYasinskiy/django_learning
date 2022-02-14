4d1cf6fc325bfc4e82a1a6f3260b1895cf93bbe1
p.yasinskiy <pachazhele@gmail.com>
2022-02-14 20:51:17 +0300

    mod 2 homework

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from random import choice
+from random import shuffle
 from django.views import View
 
 
@@ -8,9 +8,10 @@ class ToDoView(View):
     def get(self, request, *args, **kwargs):
         for_rand = ['<li>Установить python</li>', '<li>Установить django</li>', '<li>Запустить сервер</li>',
            '<li>Порадоваться результату</li>']
+        for_rand = shuffle(for_rand)
         return HttpResponse('<ul>'
-                            f'{choice(for_rand)}'
-                            f'{choice(for_rand)}'
-                            f'{choice(for_rand)}'
-                            f'{choice(for_rand)}'
+                            f'<li>{for_rand[0]}</li>'
+                            f'<li>{for_rand[1]}</li>'
+                            f'<li>{for_rand[2]}</li>'
+                            f'<li>{for_rand[3]}</li>'
                             '</ul>')
