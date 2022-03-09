a2f6b0b9e04ace2adca8998f7a431c3172fce0ea
p.yasinskiy <pachazhele@gmail.com>
2022-03-09 04:16:57 +0300

    homework 5.7

--- 04_DatabasesAndModels/board/advertisements/urls.py
+++ 04_DatabasesAndModels/board/advertisements/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import AdvertisementListView, AdvertisementDetailView
 
 urlpatterns = [
     path('advertisements/', AdvertisementListView.as_view(), name='advertisement'),
