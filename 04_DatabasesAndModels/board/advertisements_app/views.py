dc1c9ae8510a2c887ae84335dbdb29f806f2168a
p.yasinskiy <pachazhele@gmail.com>
2022-03-09 03:54:42 +0300

    homework 5.7

--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+
+class AdvertisementListView(ListView):
+    model = Advertisement
+    template_name = 'advertisement_list.html'
+    context_object_name = "advertisement_list"
+    queryset = Advertisement.objects.all()
+
+
+class AdvertisementDetailView(DetailView):
+    model = Advertisement
